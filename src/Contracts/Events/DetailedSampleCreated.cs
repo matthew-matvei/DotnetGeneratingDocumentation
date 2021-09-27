99f88eb07f21bab1dc6560bc5c75cd7ebaf0aa71
matthew-matvei <matthew.d.james87@gmail.com>
2021-09-27 22:16:42 +1000

    Provide documentation of 'detailed' types

--- src/Contracts/Events/DetailedSampleCreated.cs
+++ src/Contracts/Events/DetailedSampleCreated.cs
@@ -1,8 +1,17 @@
 ﻿namespace Contracts.Events
 {
+    /// <summary>
+    /// Demonstrates documentation for a derived type.
+    /// </summary>
+    /// <remarks>
+    /// We can show the inherited properties, and filter out <see cref="System.Object"/>
+    /// base methods
+    /// </remarks>
     public class DetailedSampleCreated : SampleCreated
     {
         public string DetailedDescription { get; set; }
+
+        /// <value>A detailed version of <see cref="SampleInfo"/>.</value>
         public new DetailedSampleInfo Sample { get; set; }
     }
 }
--- src/Contracts/Events/DetailedSampleInfo.cs
+++ src/Contracts/Events/DetailedSampleInfo.cs
@@ -2,6 +2,10 @@
 {
     public class DetailedSampleInfo : SampleInfo
     {
+        /// <value>
+        /// The only custom property of this model, everything else comes from the
+        /// underlying <see cref="SampleInfo"/>.
+        /// </value>
         public string ExtraSampleInformation { get; set; }
     }
 }
